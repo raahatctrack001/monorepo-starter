57c904210c922cd976db0addda68bb083e9d2638
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-06-01 17:48:11 +0530

    comment code for better understanding

--- apps/backend/src/controllers/auth.controller.ts
+++ apps/backend/src/controllers/auth.controller.ts
@@ -70,9 +70,11 @@ export const registerUser = asyncHandler(async (req: Request, res: Response, nex
 
 export const sendOTP = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     try {
+        //get and verify if email if is not empty
         const { email } = req.body;
         const emailCheck = emailSchema.safeParse(email);
 
+        //validate using zod
         if (!emailCheck.success) {
             const errors = (emailCheck.error as ZodError).errors.map((err: ZodIssue) => ({
                 path: err.path.join("."),  // e.g. "email"
@@ -82,15 +84,17 @@ export const sendOTP = asyncHandler(async (req: Request, res: Response, next: Ne
             throw new ApiError(403, "Error while validating email", errors);
         } 
 
-        //check if user exist as this email will be inside opt schema
+        //check if user exist as this email will be inside opt schema ::: use cache redis in future
         const user = await User.findOne({email});
         if(!user){
             throw new ApiError(404, "User with this email doesn't exist");
         }
         
+        //generate and hash otp, send simple otp in email and save hashedOTP in db
         const newOTP = generateNumericOTP();
         const otpHash = hashOTP(newOTP);
 
+        //create otp
         const otp = await Otp.create({
             userId: user?._id,
             identifier: email,
@@ -100,16 +104,19 @@ export const sendOTP = asyncHandler(async (req: Request, res: Response, next: Ne
         if(!otp){
             throw new ApiError(500, "Failed to generate OTP, please try again later!");
         }
-
+        
+        //prepare data for sending email
         const subject = "Social Project: Email Verification";
         const emailHTML = otpHtml(email, newOTP);
 
+        //send email
         const sentEmail  = await sendEmail(email, subject, emailHTML);
         const { success, message } = sentEmail;
         if(!success){
             throw new ApiError(500, message || "Failed to send otp on email, try again later");
         }
         
+        //Inform that otp has been sent!
         return res.status(201).json(new ApiResponse(201, "Email Sent Successfully, Make sure to check spam folder if not in inbox!", {}))
     } catch (error) {
         next(error)
@@ -118,6 +125,7 @@ export const sendOTP = asyncHandler(async (req: Request, res: Response, next: Ne
 
 export const verifyOTP = asyncHandler(async(req: Request, res: Response, next: NextFunction) => {
     try {
+        //extract data and validate if they are empty?
         console.log(req.body)
         const { email, otp } = req.body;
         if(!email){
@@ -142,19 +150,23 @@ export const verifyOTP = asyncHandler(async(req: Request, res: Response, next: N
             throw new ApiError(403, "Please enter valid otp");
         }
         
+        //check if user exists :: user cached, redis in future
         const user = await User.findOne({email});
         if(!user){
             throw new ApiError(404, "User with this email doesn't exist");
         }
         
+        //collective data
         const allCodes = await Otp.find({userId: user?._id, identifier: email});
         const latestHashedOTP = allCodes.at(-1);
         
+        //hash user otp to compare with otpHash in db
         const userHashedOTP = hashOTP(otp);
         if(userHashedOTP !== latestHashedOTP?.otpHash){
             throw new ApiError(403, "Please enter valid OTP")
         }
 
+        //update user that email has been verified
         const updateUser = await User.findOneAndUpdate({email}, {
             $set: {
                 emailVerified: true,
