43190c6739ca57cec4476357c5d1d091923adade
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-06-02 17:17:15 +0530

    add type for req.user globally

--- apps/backend/src/types/express/index.d.ts
+++ apps/backend/src/types/express/index.d.ts
@@ -1,9 +1,17 @@
+import { IUser } from '@repo/database';
 import { JwtPayload } from 'jsonwebtoken';
+
+interface UserPayload extends JwtPayload {
+  _id: IUser["_id"];
+  fullName: string;
+  username: string;
+}
+
 
 declare global {
   namespace Express {
     interface Request {
-      user?: string | JwtPayload;
+      user?: UserPayload & JwtPayload;
     }
   }
 }
