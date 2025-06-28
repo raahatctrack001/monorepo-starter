72f8275c10ee4af8d5225cc9091fbac00fed1171
Raahat Khan <raahatctrack001.rc@gmail.com>
2025-06-28 23:25:48 +0530

    fix: remove connection string logs to avoid leaking sensitive info:

--- packages/database/src/mongodb.connection.ts
+++ packages/database/src/mongodb.connection.ts
@@ -13,11 +13,11 @@ export const databaseConnection = async (): Promise<Connection | void> => {
     // console.log("mongodb connection url from mongodb.connection.ts", process.env.MONGODB_CONNECTION_STRING)
     try  {                   
         const remoteConnectionString = process.env.MONGODB_CONNECTION_STRING;
-        const localConnectionString = process.env.MONGODB_CONNECTION_STRING_DOCKER;
-        const connectionString = process.env.NODE_ENV !== 'production' ? remoteConnectionString : localConnectionString
-        console.log('All environment variables:', Object.keys(process.env));
-        console.log('MongoDB string:', process.env.MONGODB_CONNECTION_STRING);
-        console.log('MongoDB string length:', process.env.MONGODB_CONNECTION_STRING?.length);
+        // const localConnectionString = process.env.MONGODB_CONNECTION_STRING_DOCKER;
+        // const connectionString = process.env.NODE_ENV !== 'production' ? remoteConnectionString : localConnectionString
+        // console.log('All environment variables:', Object.keys(process.env));
+        // console.log('MongoDB string:', process.env.MONGODB_CONNECTION_STRING);
+        // console.log('MongoDB string length:', process.env.MONGODB_CONNECTION_STRING?.length);
                 
         const connectionInstance = await mongoose.connect(remoteConnectionString);
         return connectionInstance.connection;
